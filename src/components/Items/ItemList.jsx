828de2413b66adcd9ab4e10030df12777d82d90d
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-09-12 12:45:29 -0300

    primera entrega pero con mas estilos

--- src/components/Items/ItemList.jsx
+++ src/components/Items/ItemList.jsx
@@ -2,11 +2,54 @@ import React from 'react'
 
 const ItemList = ({ product }) => {
     return (
-        <div>
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-            <p>{product.stock}</p>
+
+
+        <div className="flex font-sans w-52">
+            <div className="flex-none w-48 relative">
+                <img src={product.img} alt="" class="absolute inset-0 w-full h-full object-cover" loading="lazy" />
+            </div>
+            <form className="flex-auto p-6">
+                <div className="flex flex-wrap">
+                    <h1 className="flex-auto text-lg font-semibold text-slate-900">
+                        {product.name}
+                    </h1>
+                    <div className="text-lg font-semibold text-slate-500">
+                       $ {product.price}
+                    </div>
+                    <div className="w-full flex-none text-sm font-medium text-slate-700 mt-2 py-7">
+                        <p> Stock : {product.stock} </p>
+                    </div>
+                </div>
+
+                <div className="flex space-x-4 mb-6 text-sm font-medium">
+                    <div className="flex-auto flex space-x-4">
+                        <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                            +
+                        </button>
+                        <button clasNames="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+                            -
+                        </button>
+                    </div>
+                    <button className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like">
+                        <svg width="20" height="20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+                        </svg>
+                    </button>
+                </div>
+                <p className="text-sm text-slate-700">
+                    Free shipping
+                </p>
+            </form>
         </div>
+
+
+
+
+        //  <div>
+        //   <p>{product.name}</p>
+        //   <p>{product.price}</p>
+        //  <p>{product.stock}</p>
+        // </div>
     )
 }
 
--- src/components/Items/ItemListContainer.jsx
+++ src/components/Items/ItemListContainer.jsx
@@ -19,12 +19,12 @@ const ItemListContainer = () => {
     // Datos de productos ( simulados)
 
     const productos = [
-        { id: 1, name: "Hermione", price: 10.99, stock: 50 },
-        { id: 2, name: "Luna", price: 19.99, stock: 30 },
-        { id: 3, name: "Snape", price: 5.99, stock: 100 },
-        { id: 4, name: "Charmander", price: 25.99, stock: 20 },
-        { id: 5, name: "Pidgeotto", price: 14.49, stock: 60 },
-        { id: 6, name: "Baby Yoda", price: 8.99, stock: 75 },
+        { id: 1, name: "Hermione", price: 10.99, stock: 50, img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/003/029/560/products/funko-pop-harry-potter-hermione-granger-1131-30fcb935fdf6a044b116826950842502-640-0.jpg"},
+        { id: 2, name: "Luna", price: 19.99, stock: 30 , img: "https://http2.mlstatic.com/D_NQ_NP_612191-MLA43657068710_102020-O.webp" },
+        { id: 3, name: "Snape", price: 5.99, stock: 100, img: "https://i.pinimg.com/564x/9c/d5/7b/9cd57b7498c5bd06d079885608596b03.jpg"},
+        { id: 4, name: "Charmander", price: 25.99, stock: 20 , img: "https://i0.wp.com/animalscollectibles.com.ar/wp-content/uploads/2023/03/char.jpg?fit=1000%2C1000&ssl=1" },
+        { id: 5, name: "Pidgeotto", price: 14.49, stock: 60, img: "https://http2.mlstatic.com/D_NQ_NP_780270-MLA49511308881_032022-O.webp" },
+        { id: 6, name: "Baby Yoda", price: 8.99, stock: 75, img: "https://funko.com.ar/wp-content/uploads/2023/03/405-grogu-2.jpg" },
     ]
 
     // Funcion que simula una llamada asincrona a una API o servidor
@@ -72,7 +72,7 @@ const ItemListContainer = () => {
             {
                 // mapeamos los datos de productos ( cuando se resolvio todo exitosamente) y renderizamos cada uno usando el componente ItemList
                 data.map((el) => (
-                    <ItemList key={el.id} product={el}/>
+                    <ItemList key={el.id} product={el} />
                 ))
             }
 
