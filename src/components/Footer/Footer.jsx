7d3a9a49a0134d218a8ba5846cb434f7fab49891
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-10-30 18:27:27 -0300

    arreglo de css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,11 @@ import './footer.css'
 const Footer = () => {
     return (
 
-        <div className='footer'>
+        <div className='footer bg-slate-500'>
             <ul className='lista'>
                 <li>Contacto</li>
                 <li>Ubicacion</li>
-                <li>Copyright</li>
+                <li>Copyright ©</li>
             </ul>
         </div>
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import CartWidget from '../CartWidget/CartWidget'
 import { Link } from 'react-router-dom'
+import "./Navbar.css"
 
 
 
@@ -9,20 +10,20 @@ const Navbar = () => {
 
 
     return (
-        <div className='flex items-center justify-between px-10 py-5 bg-slate-400'>
+        <div className='flex items-center justify-between px-10 py-7 bg-slate-500'>
             <Link to={'/'}> <img className='w-60' src="/src/multimedia/branding/logo_light_horizontal.svg " alt="" /> </Link>
             <ul className='flex'>
-                <li className='mx-5 text-zinc-50'>
+                <li className='mx-7 text-zinc-50 textNavbar'>
                     <Link to={'/'}> Home </Link>
                 </li>
-                <li className='mx-5 text-zinc-50'>
+                <li className='mx-7 text-zinc-50 textNavbar'>
                     <Link to={'/items'}> Catalogo </Link>
                 </li>
-                <li className='mx-5 text-zinc-50'> <Link to={'/items/Pokemon'}>Pokemon</Link>
+                <li className='mx-7 text-zinc-50 textNavbar'> <Link to={'/items/Pokemon'}>Pokemon</Link>
                 </li>
-                <li className='mx-5 text-zinc-50'> <Link to={'/items/Star-Wars'}>Star Wars</Link>
+                <li className='mx-7 text-zinc-50 textNavbar'> <Link to={'/items/Star-Wars'}>Star Wars</Link>
                 </li>
-                <li className='mx-5 text-zinc-50'> <Link to={'/items/Hogwarts'}>Harry Potter</Link>
+                <li className='mx-7 text-zinc-50 textNavbar'> <Link to={'/items/Hogwarts'}>Harry Potter</Link>
                 </li>
 
             </ul>
